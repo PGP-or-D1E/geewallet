7757485f766a2be33450171717f2fbc323180534
Andrew Cann <shum@canndrew.org>
2020-07-10 14:28:05 +0800

    LN: add support for monohop unidirectional payments
    Co-authored-by: Andres G. Aragoneses <knocte@gmail.com>

--- src/GWallet.Backend/PublicKey.fs
+++ src/GWallet.Backend/PublicKey.fs
@@ -3,12 +3,22 @@ namespace GWallet.Backend
 
 open NBitcoin
 
-type PublicKey(pubKey: string, currency: Currency) =
+type PublicKey private (pubKey: string, currencyOpt: Option<Currency>) =
     do
-        if currency.IsUtxo() then
-            PubKey pubKey
-            |> ignore<PubKey>
+        match currencyOpt with
+        | Some currency when currency.IsUtxo() ->
+            // just for validation
+            PubKey pubKey |> ignore
+        | _ ->
+            // TODO: validate ether keys too
+            ()
+
+    internal new(pubKey: PubKey) = PublicKey(pubKey.ToString(), None)
+    internal new(pubKey: string, currency: Currency) = PublicKey(pubKey.ToString(), Some currency)
 
     override __.ToString() =
         pubKey
+
+    static member internal Parse (currency: Currency) (pubKey: string): PublicKey =
+        PublicKey (pubKey, currency)
 
